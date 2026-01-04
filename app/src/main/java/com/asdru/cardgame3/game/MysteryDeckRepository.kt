114d8099e02deb42d9db21d0c738de1dd382bf65
asdru22 <76167257+asdru22@users.noreply.github.com>
2026-01-04 19:01:58 +0100

    more mystery cards

--- app/src/main/java/com/asdru/cardgame3/game/MysteryDeckRepository.kt
+++ app/src/main/java/com/asdru/cardgame3/game/MysteryDeckRepository.kt
@@ -1,8 +1,11 @@
 package com.asdru.cardgame3.game
 
+import androidx.compose.ui.util.fastForEachReversed
 import com.asdru.cardgame3.R
 import com.asdru.cardgame3.data.MysteryCard
 import com.asdru.cardgame3.helper.heal
+import com.asdru.cardgame3.helper.receiveDamage
+import com.asdru.cardgame3.logic.BattleCombatLogic.performActiveAbility
 import kotlin.random.Random
 
 object MysteryDeckRepository {
@@ -17,7 +20,7 @@ object MysteryDeckRepository {
     MysteryCard(
       descriptionRes = R.string.mc_desc_heal,
       formatArgs = listOf(5f),
-      weight = 30,
+      weight = 50,
       onApply = { team ->
         team.aliveEntities.forEach { it.heal(5f, it) }
       }
@@ -41,9 +44,48 @@ object MysteryDeckRepository {
     MysteryCard(
       descriptionRes = R.string.mc_desc_decrease_rage_e,
       formatArgs = listOf(13f),
-      weight = 10,
+      weight = 40,
       onApply = { team ->
         team.enemyTeam.decreaseRage(13f)
+      }
+    ),
+    MysteryCard(
+      descriptionRes = R.string.mc_desc_deal_damage_e,
+      formatArgs = listOf(6f),
+      weight = 40,
+      onApply = { team ->
+        team.enemyTeam.getTargetableEnemies().forEach { it.receiveDamage(6f) }
+      }
+    ),
+    MysteryCard(
+      descriptionRes = R.string.mc_desc_use_active,
+      weight = 30,
+      onApply = { team ->
+        team.getAliveMembers().randomOrNull()?.let { attacker ->
+          team.getTargetableEnemies().randomOrNull()?.let { target ->
+            performActiveAbility(attacker, target)
+          }
+        }
+      }
+    ),
+    MysteryCard(
+      descriptionRes = R.string.mc_desc_increase_effect_duration,
+      formatArgs = listOf(1),
+      weight = 25,
+      onApply = { team ->
+        team.getAliveMembers().forEach {
+          it.effectManager.effects.forEach { effect -> effect.duration += 1 }
+        }
+      }
+    ),
+    MysteryCard(
+      descriptionRes = R.string.mc_desc_increase_effect_duration_e,
+      formatArgs = listOf(1),
+      weight = 25,
+      onApply = { team ->
+        team.getAliveEnemies().forEach {
+          it.effectManager.effects.forEach { effect -> effect.duration += 1 }
+        }
       }
     )
   )
